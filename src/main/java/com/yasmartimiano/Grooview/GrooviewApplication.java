0fa2aa6e7a56c182663510ba9880677ae8bad342
yasmartimiano <yasminliver56@gmail.com>
2025-08-24 22:36:44 -0300

    first commit

---
+++ src/main/java/com/yasmartimiano/Grooview/GrooviewApplication.java
@@ -0,0 +1,13 @@
+package com.yasmartimiano.Grooview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasmartimiano/Grooview/GrooviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasmartimiano.Grooview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrooviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
